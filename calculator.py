f649b6429f18ed6608e8abe5029f66793e9ed625
Monisha Jacob <monishacherian@gmail.com>
2020-10-01 00:12:40 +0000

    all functions finished

--- calculator.py
+++ calculator.py
@@ -5,3 +5,29 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 
 
 # Replace this with your code
+while True:
+    user_input = input ("Enter your Equation : ")
+    tokens = user_input.split(" ")
+    print (tokens)
+    if tokens[0] == "q":
+        print("ITS BROKEN")
+        break
+    else:
+        token1 = int(tokens[1])
+        token2 = int(tokens[2])
+        if tokens[0] == "+":
+            print(add(token1, token2))
+        elif tokens[0] == "-":
+            subtract(token1, token2)
+        elif tokens[0] == "*":
+            multiply(token1, token2)
+        elif tokens[0] == "/":
+            divide(token1, token2)
+        elif tokens[0] == "square":
+            square(token1)
+        elif tokens[0] == "cube":
+            cube(token1)
+        elif tokens[0] == "pow":
+            power(token1, token2)
+        elif tokens[0] == "mod":
+            mod(token1, token2)
